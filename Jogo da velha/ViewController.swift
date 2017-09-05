dd35b554af4ea5f2277fcd82fd010ca077490a4d
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-09-05 00:28:27 +0000

    Mostrando na tela quem ganhou e implementação do button Jogar Novamente

--- Jogo da velha/ViewController.swift
+++ Jogo da velha/ViewController.swift
@@ -20,6 +20,29 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var button: UIButton!
     
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    @IBOutlet weak var gameOverLabel: UILabel!
+    
+    @IBAction func playAgainPressed(_ sender: AnyObject) {
+        activePlayer = 1
+        gameActive = true
+        gameState = [0,0,0,0,0,0,0,0,0]
+        
+        gameOverLabel.isHidden = true
+        playAgainButton.isHidden = true
+        
+        var button: UIButton
+        
+        for i in 0 ..< 9 {
+            button = view.viewWithTag(i) as! UIButton
+            button.setImage(nil, for: .normal)
+        }
+        
+        gameOverLabel.center = CGPoint(x: gameOverLabel.center.x - 500, y :gameOverLabel.center.y)
+        playAgainButton.center = CGPoint(x: playAgainButton.center.x - 500, y :playAgainButton.center.y)
+    }
+    
     @IBAction func buttonPressed(_ sender: AnyObject) {
         
         if gameState[sender.tag] == 0 && gameActive == true {
@@ -40,12 +63,22 @@ class ViewController: UIViewController {
             for combination in winCombination {
                 
                 if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
-                    print("Temos um vencedor")
+                    
                     var winner = "O Ganhou"
                     
                     if gameState[combination[0]] == 2 {
                         winner = "X Ganhou"
                     }
+                    
+                    gameOverLabel.text = winner
+                    
+                    gameOverLabel.isHidden = false
+                    playAgainButton.isHidden = false
+                    
+                    UIView.animate(withDuration: 0.5, animations: {
+                        self.gameOverLabel.center = CGPoint(x: self.gameOverLabel.center.x + 500, y :self.gameOverLabel.center.y)
+                        self.playAgainButton.center = CGPoint(x: self.playAgainButton.center.x - 500, y: self.playAgainButton.center.y)
+                    })
                     
                     print(winner)
                     gameActive = false
@@ -58,7 +91,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        gameOverLabel.isHidden = true
+        playAgainButton.isHidden = true
+        
+        //gameOverLabel.center = CGPoint(x: gameOverLabel.center - 500, gameOverLabel.center.y)
+        
+        gameOverLabel.center = CGPoint(x: gameOverLabel.center.x - 500, y :gameOverLabel.center.y)
+        playAgainButton.center = CGPoint(x: playAgainButton.center.x - 500, y :playAgainButton.center.y)
+        
     }
 
     override func didReceiveMemoryWarning() {
